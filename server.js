e6702d8a95df017a8628723d7d9dfb73100575f1
Corbin Taylor <Corbin.Taylor@DrakeSoftware.com>
2022-08-30 10:25:31 -0400

    installed handlebars for front-side instead of buildling separate front end as its a small app. pausing for now.

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const express = require('express');
 const mongoose = require('mongoose'); 
 const PORT = process.env.PORT || 5000; 
 const routes = require('./routes');
-
+const exphbs = require("express-handlebars");
 mongoose.connect(`${process.env.MONGOURI}`, {
     useNewUrlParser: true,
   });
 
-const app = express();
+const app = express();  
+app.engine('handlebars', exphbs.engine());
+app.set("view engine", "handlebars");
+app.set('views', './Views');
+app.get('/', (req, res) => {
+    res.render('home');
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
